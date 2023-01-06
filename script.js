c58cc263d1c0fe9d4514ba47efff2250ded151cc
subrata410 <subrata.rooy2022@gmail.com>
2023-01-06 20:40:16 +0600

    JavaScript Class Eight

---
+++ script.js
@@ -0,0 +1,111 @@
+// Function calling other function
+// const calculateAge = function (birthYear) {
+//     return 2022 - birthYear;
+// }
+
+// // const aAge = calculateAge(1975);
+// // console.log(aAge);
+// const jobLeft = function (birthYear) {
+//     const age = calculateAge(birthYear);
+//     return 65 - age;
+// };
+
+// const ajobLeft = jobLeft(1975);
+// console.log(ajobLeft);
+
+// JavaScript Events
+// function displayText() {
+//     console.log("This is Conselt text!");
+// }
+
+// function alertText() {
+//     alert("This is Alert Text");
+// }
+
+
+// JavaScript Strings
+// const fastName = "Asif";
+// const lastName = 'Akber';
+// console.log(fastName);
+// console.log(lastName);
+
+// // Example
+// const text = 'I love "Bangladesh!"';
+// console.log(text);
+
+// // Example 2
+// const text2 = "I love 'Bangladesh!'";
+// console.log(text2);
+
+// // Example 3
+// const text3 = "I love \"Bangladesh!\"";
+// console.log(text3);
+
+// // Example 4
+// const text4 = "I love \\ Bangladesh!";
+// console.log(text4);
+
+// Strings Methods
+// const x = "Subrata Roy";
+// console.log(x.length);
+
+// // String Slice
+// const fruits = "Apple, Banana, Mango";
+// console.log(fruits);
+
+// const fruit1 = fruits.slice(0, 5);
+// console.log(fruit1);
+
+// const fruit2 = fruits.slice(7, 13);
+// console.log(fruit2);
+
+// const fruit3 = fruits.slice(15, 20);
+// console.log(fruit3);
+
+// console.log(fruit1, fruit2, fruit3);
+
+// Negetive Slice
+// const fruits = "Apple, Banana, Mango";
+
+// const fruit1 = fruits.slice(-5);
+// console.log(fruit1);
+
+// const fruit2 = fruits.slice(-13, -7);
+// console.log(fruit2);
+
+// // substrings
+// const fruits = "Apple, Banana, Mango";
+// const fruit1 = fruits.substring(0, 5);
+// console.log(fruit1);
+
+// substr
+
+// // Strings replace
+// const text = "I love Khulna!";
+// console.log(text);
+// const text2 = text.replace("Khulna", "Dhaka");
+// console.log(text2);
+
+// Strings toUpperCase
+// const text = "Hello Word!";
+// console.log(text);
+// console.log(text.toUpperCase());
+
+// Strings toLowerCase
+// const text = "Hello Word!";
+// console.log(text);
+// console.log(text.toLowerCase());
+
+// Strings trim
+// const text = "      Hello Word!       ";
+// console.log(text);
+// console.log(text.trim());
+
+
+// Strings Search
+// const text = "Rohim Karim Sakib Karim";
+// console.log(text.indexOf("Karim"));
+
+// console.log(text.lastIndexOf("Karim"))
+// console.log(text.search("Karim"))
+
